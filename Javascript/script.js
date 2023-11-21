e4c2d2bbdf40f02e84ef67115ce8a278ecd2b5bd
HonzaKanovsky <honza@kanovsky.net>
2023-11-21 23:01:51 +0100

    script.js deleted

--- Javascript/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-function showItem(itemId) {
-    // Assuming productDetail.html exists for each item
-    var productDetailUrl = '../detail_product.html?item=' + itemId;
-    window.location.href = productDetailUrl;
-}
