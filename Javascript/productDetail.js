c7e09b66c6329b1ff1893bb5896f4aeb4a59199e
HonzaKanovsky <honza@kanovsky.net>
2023-11-21 23:01:11 +0100

    Product detail changes

--- Javascript/productDetail.js
+++ Javascript/productDetail.js
@@ -1,47 +1,106 @@
-// productDetail.js
-document.addEventListener('DOMContentLoaded', function () {
-    var urlParams = new URLSearchParams(window.location.search);
-    var itemId = urlParams.get('item');
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.carousel-slide');
+const firstSlide = slides[0]; // Get the first slide
 
-    // Fetch and display product details
-    var productDetails = getProductDetails(itemId);
-    var imageUrl = "img/RI048b2.webp"
-    var productName = getProductName(itemId);
+// Show only the first slide initially
+firstSlide.style.display = 'block';
 
-    if (!itemId || !productDetails) {
-        // If item ID is not found or details are empty, show 404 message
-        document.getElementById('item-description').innerHTML = '<H1>error</H1>\n<p>Page Not Found</p>';
-    } else {
-        console.log("Product Name:", productName);
-        console.log("Image URL:", imageUrl);
-        console.log("Product Details:", productDetails);
-        // Display the item ID and product details
-        document.getElementById('item-name').innerHTML = '<h1>' + productName + '</h1>\n';
-        document.getElementById('item-image').src = imageUrl;
-        document.getElementById('item-description').innerHTML = '<p>' + productDetails + '</p>';
-    }
+// Hide all other slides
+for (let i = 1; i < slides.length; i++) {
+  slides[i].style.display = 'none';
+}
+
+let currentSlide = 0; // Track the current slide index
+
+const prevButton = document.querySelector('.prev');
+prevButton.addEventListener('click', () => {
+  if (currentSlide > 0) {
+    // Hide the current slide
+    slides[currentSlide].style.display = 'none';
+
+    // Update the current slide index
+    currentSlide--;
+
+    // Show the previous slide
+    slides[currentSlide].style.display = 'block';
+  }
 });
 
-// Mock function to fetch product details (replace this with actual data fetching logic)
-function getProductDetails(itemId) {
-    // Mock data, replace with actual data fetching logic
-    var details = {
-        'item1': 'Mobilní telefon - 6,1" Super Retina XDR OLED 2532 \× 1170, operační paměť 4 GB, vnitřní paměť 128 GB, single SIM + eSIM, procesor Apple A15 Bionic, fotoaparát: 12Mpx (f/1,6) hlavní + 12Mpx širokoúhlý, přední kamera 12Mpx, GPS, NFC, LTE, 5G, Lightning port, voděodolný dle IP68, rychlé nabíjení 20W, bezdrátové nabíjení 15W, baterie 3240 mAh, model 2021, iOS',
-        'item2': 'Awesome details for Item 2.',
-        // Add more details as needed
-    };
+const nextButton = document.querySelector('.next');
+nextButton.addEventListener('click', () => {
+  if (currentSlide < slides.length - 1) {
+    // Hide the current slide
+    slides[currentSlide].style.display = 'none';
 
-    return details[itemId];
+    // Update the current slide index
+    currentSlide++;
+
+    // Show the next slide
+    slides[currentSlide].style.display = 'block';
+  }
+});
+
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+const links = document.querySelectorAll('a[href="#reviews-content"]');
+
+for (const tab of tabs) {
+  tab.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default click behavior
+
+    const target = event.target.getAttribute('data-target');
+
+    // Remove the active class from the previously active tab
+    const previouslyActiveTab = document.querySelector('.tab.active');
+    if (previouslyActiveTab) {
+      previouslyActiveTab.classList.remove('active');
+    }
+
+    // Hide all tab contents
+    for (const tabContent of tabContents) {
+      tabContent.classList.remove('active');
+      tabContent.style.display = 'none';
+    }
+
+    // Show the selected tab content
+    const selectedTabContent = document.querySelector(`#${target}`);
+    selectedTabContent.classList.add('active');
+    selectedTabContent.style.display = 'block';
+
+    // Add the active class to the newly selected tab
+    tab.classList.add('active');
+  });
 }
 
-// Mock function to fetch product details (replace this with actual data fetching logic)
-function getProductName(itemId) {
-    // Mock data, replace with actual data fetching logic
-    var details = {
-        'item1': 'Ifon',
-        'item2': 'Telefon',
-        // Add more details as needed
-    };
+for (const link of links) {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior
 
-    return details[itemId];
+    const target = link.getAttribute('href');
+    const correspondingTab = document.querySelector(`[data-target="${target}"]`);
+
+    correspondingTab.click(); // Trigger click event on the corresponding tab
+  });
 }
+const reviewsLink = document.getElementById('reviewsLink');
+
+reviewsLink.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const reviewsTab = document.querySelector('[data-target="reviews-content"]');
+  reviewsTab.click(); // Trigger click event on the Reviews tab
+});
+
+const addToCartButtonProduct = document.querySelector('.add-to-cart-btn');
+
+addToCartButtonProduct.addEventListener('click', () => {
+  window.location.href = '/kosik.html';
+});
+
+const addToCartButtons = document.querySelectorAll('.Homepage-product button.Homepage-add-to-cart');
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    window.location.href = '/kosik.html';
+  });
+});
